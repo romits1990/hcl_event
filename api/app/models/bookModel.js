12c9c8cbb957d7a6869c951de1ca82003653e5f5
romitCareClues <romit.sanyal@careclues.com>
2021-11-27 11:34:26 +0530

    data modelling and seeding done

---
+++ api/app/models/bookModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bookSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        author: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        category: {
+            type: String,
+            required: true,
+            trim: true,
+            default: 'books detective thriller ghost romantic scifi'
+        },
+        fee: {
+            type: Number,
+            required: true,
+            trim: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const model = mongoose.model('Book', bookSchema);
+module.exports = model;
--- api/seeders/seed.js
+++ api/seeders/seed.js
@@ -11,54 +11,18 @@ dbConnection.on('open', async () => {
         dbConnection.dropDatabase();
         debug(`Database connected successfully.`);
         debug('Seeding started');
-        await startSeeding();
-        debug('Seeding finished');
-        dbConnection.close();
+        const BookModel = require(`../app/models/BookModel`);
+        const seedSource = require('./seedSource.json');
+        seedSource.forEach(async (item, index, mainList) => {
+            const book = new BookModel(item);
+            await book.save({validateBeforeSave: false});
+            if(index+1===mainList.length) {
+                process.exit();
+            }
+        });
     }
     catch(error) {
         debug(error);
         process.exit();
     }
 });
-dbConnection.once('error', () => { debug('Database connection error.'); });
-dbConnection.on('disconnected', () => { debug('Database disconnected successfully.'); });
-
-function startSeeding() {
-    return new Promise((resolve, reject) => {
-        const seedSource = require('./seedSource.json');
-        let userRole;
-        seedSource.forEach((seedItem, seedIndex) => {
-            const currentSeedItemIndex = seedIndex + 1;
-            const model = require(`../app/models/${seedItem.model}Model`);
-            const data = seedItem.data;
-            data.forEach(async (item, index) => {
-                let currentIndex = index + 1;
-                try {
-
-                    if(typeof userRole!=='undefined' && seedItem.model==='user') {
-                        item.role = userRole._id;
-                    }
-
-                    const newData = new model(item);
-
-                    if(seedItem.model==='role') {
-                        userRole = newData;
-                    }
-                    
-                    await newData.save({validateBeforeSave: false});
-
-                    if(currentIndex===data.length) {
-                        debug(`${data.length} ${seedItem.model} seeded`);
-                        if(currentSeedItemIndex===seedSource.length) {
-                            resolve();
-                        }
-                    }
-                }
-                catch(error) {
-                    debug(error);
-                    resolve();
-                }
-            });
-        });
-    })
-}
